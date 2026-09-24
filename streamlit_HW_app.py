814da70268c75d75ad14fd18740673146a9e8510
Sherwin1429 <smurzell@syr.edu>
2026-09-24 20:39:59 +0000

    changes

--- streamlit_HW_app.py
+++ streamlit_HW_app.py
@@ -18,16 +18,21 @@ hw2 = st.Page(
 
 hw3 = st.Page(
     "HW/HW3.py",
-    title="Homework 3",
+    title="Homework 3"
    
 )
 
 hw4 = st.Page(
     "HW/HW4.py",
-    title="Homework 4",
+    title="Homework 4"
+)
+
+hw5 = st.Page(
+    "HW/HW5.py",
+    title="Homework 5",
     default=True
 )
 
-pg = st.navigation([hw1, hw2, hw3, hw4])
+pg = st.navigation([hw1, hw2, hw3, hw4, hw5])
 
 pg.run()
